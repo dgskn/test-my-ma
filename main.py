1327393428ec6a7beda0a0ee669629f6db127563
dgskn <46501685+dgskn@users.noreply.github.com>
2025-09-27 15:40:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ TARGET_ORIGIN = os.getenv("TARGET_ORIGIN")
 # Загружаем куки аккаунтов из окружения
 ACCOUNTS = [
     {"name": "Аккаунт 1", "cookie": os.getenv("ACCOUNT_1_COOKIE")},
-    #{"name": "Аккаунт 2", "cookie": os.getenv("ACCOUNT_2_COOKIE")},
+    {"name": "Аккаунт 2", "cookie": os.getenv("ACCOUNT_2_COOKIE")},
     {"name": "Аккаунт 3", "cookie": os.getenv("ACCOUNT_3_COOKIE")},
     {"name": "Аккаунт 4", "cookie": os.getenv("ACCOUNT_4_COOKIE")},
     {"name": "Аккаунт 5", "cookie": os.getenv("ACCOUNT_5_COOKIE")},
@@ -284,3 +284,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
