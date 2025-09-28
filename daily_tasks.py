9047b794873e8ac89ad3aba7cfa7e5d85a234945
dgskn <46501685+dgskn@users.noreply.github.com>
2025-09-28 17:31:16 +0300

    Update daily_tasks.py

--- daily_tasks.py
+++ daily_tasks.py
@@ -12,7 +12,7 @@ QUEST_API_URL = os.getenv("QUEST_API_URL")
 TARGET_ORIGIN = os.getenv("TARGET_ORIGIN")
 ACCOUNTS = [
     {"name": "Аккаунт 1", "cookie": os.getenv("ACCOUNT_1_COOKIE")},
-    #{"name": "Аккаунт 2", "cookie": os.getenv("ACCOUNT_2_COOKIE")},
+    {"name": "Аккаунт 2", "cookie": os.getenv("ACCOUNT_2_COOKIE")},
     {"name": "Аккаунт 3", "cookie": os.getenv("ACCOUNT_3_COOKIE")},
     {"name": "Аккаунт 4", "cookie": os.getenv("ACCOUNT_4_COOKIE")},
     {"name": "Аккаунт 5", "cookie": os.getenv("ACCOUNT_5_COOKIE")},
